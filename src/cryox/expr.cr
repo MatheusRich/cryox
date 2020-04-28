b8cbff3d7f20a08cf783ee4c22ba1f7bc90c0946
Matheus Richard <matheusrichardt@gmail.com>
2020-04-27 23:20:59 -0300

    Generate expr classes

--- src/cryox/expr.cr
+++ src/cryox/expr.cr
@@ -8,4 +8,23 @@ abstract class Cryox::Expr
 
     def initialize(@left, @operator, @right); end
   end
+
+  class Grouping < Expr
+    getter expression : Expr
+
+    def initialize(@expression); end
+  end
+
+  class Literal < Expr
+    getter value : Union(String | Float64 | Nil)
+
+    def initialize(@value); end
+  end
+
+  class Unary < Expr
+    getter operator : Token
+    getter right : Expr
+
+    def initialize(@operator, @right); end
+  end
 end
