f99424a9160475d5cd638bed0de4be4c9869b1d8
Matheus Richard <matheusrichardt@gmail.com>
2020-11-05 16:44:08 -0300

    Refactor test

--- spec/cryox/lox_spec.cr
+++ spec/cryox/lox_spec.cr
@@ -4,14 +4,8 @@ require "stdio"
 describe Cryox::Lox do
   describe ".run_file" do
     it "manages scope" do
-      output = Stdio.capture do |io|
-        Cryox::Lox.run_file("spec/fixtures/scope.lox")
-
-        io.out.gets_to_end
-      end
-
       expected_scope = "inner a\nouter b\nglobal c\nouter a\nouter b\nglobal c\nglobal a\nglobal b\nglobal c\n"
-      output.should eq expected_scope
+      output_of { Cryox::Lox.run_file("spec/fixtures/scope.lox") }.should eq expected_scope
     end
   end
 end
--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -1,2 +1,9 @@
 require "spec"
 require "../src/cryox"
+
+def output_of
+  Stdio.capture do |io|
+    yield
+    io.out.gets_to_end
+  end
+end
